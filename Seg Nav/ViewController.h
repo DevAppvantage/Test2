7b67efe561028547d409a83087219538de33baf9
Barima Kwarteng <barima@appvantagemobile.com>
2012-05-22 16:50:37 -0600

    Initial Commit

---
+++ Seg Nav/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Seg Nav
+//
+//  Created by Barima Kwarteng on 5/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
